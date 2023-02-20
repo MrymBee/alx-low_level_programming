453d7679c85224b137ddac2d34fcefcfc2a73bed
MrymBee <maryamnikee@gmail.com>
2023-02-20 02:29:58 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry Point
- * Return: 1 (complete)
+ * main - Entry
+ * return: 1 (complete)
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-Return(1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	Return(1);
 }
