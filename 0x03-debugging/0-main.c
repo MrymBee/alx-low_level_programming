b47ff4c8f04349963a9c2992fe4f5ca04f08c051
MrymBee <maryamnikee@gmail.com>
2023-02-24 13:20:31 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -positive or negative for test functions
+ * Return: 0 (complete)
+ */
+
+int main(void)
+{
+ int i;
+
+ i = 0;
+ positive_or_negative(i);
+
+return (0);
+}
